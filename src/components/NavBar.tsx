612083439fad30962118bf93d1eec37c2338efb6
L <email.linch@gmail.com>
2024-03-30 22:20:43 -0700

    Rename Pricing to About Us

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,7 +16,7 @@ const sections = [
   },
   { id: 'screencasts', title: 'Screencasts' },
   { id: 'resources', title: 'Resources' },
-  { id: 'pricing', title: 'Pricing' },
+  { id: 'pricing', title: 'About Us' },
   { id: 'author', title: 'Author' },
 ]
 
